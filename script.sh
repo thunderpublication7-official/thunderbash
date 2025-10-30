88b0800f70de03ee24bd4661a07a4ceb6c2113cb
thunderpublication7-official <thunderpublication218@gmail.com>
2025-10-31 00:37:00 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,8 +2,7 @@ echo "hello, this custom script is running! this is for debian"
 pkg update && apt upgrade -y
 pkg install proot-distro -y
 echo "which distro would you like to install"
-read os
-pd login $os
+pd login debian
 apt update && apt upgrade -y
 apt install sudo -y
 
