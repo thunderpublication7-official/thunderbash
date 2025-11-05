ee18ba72ba6f8a44d584fcf18de9ba6f0aa67817
thunderpublication7-official <thunderpublication218@gmail.com>
2025-11-05 21:49:09 +0530

    Enhance script with additional package installations
    Added installation of figlet and viu, and included commands to download and display a logo.

--- script.sh
+++ script.sh
@@ -1,6 +1,11 @@
 echo "hello, this custom script is running! this is for debian"
 pkg update -y
 apt upgrade -y
+pkg install figlet
+pkg install viu
+wget https://raw.githubusercontent.com/thunderpublication7-official/thunder-bash-site/main/logo_thundershell.png
+viu logo_thundershell.png
+echo -e "\033[0;32m$(figlet 'thunder shell')\033[0m"
 pkg install proot-distro -y
 pd install debian
 pd login debian
