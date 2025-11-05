29139e53f67a1519a91995a5ea743c45783163f8
thunderpublication7-official <thunderpublication218@gmail.com>
2025-11-05 14:19:22 +0530

    Refactor script.sh for package updates and installation

--- script.sh
+++ script.sh
@@ -1,10 +1,8 @@
 echo "hello, this custom script is running! this is for debian"
-pkg update && apt upgrade -y
+pkg update -y
+apt upgrade -y
 pkg install proot-distro -y
-echo "which distro would you like to install"
+pd install debian
 pd login debian
-apt update && apt upgrade -y
-apt install sudo -y
-
 
 
