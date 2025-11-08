d08e0493b520c652feb4f672a7b48fc4076c65c4
thunderpublication7-official <thunderpublication218@gmail.com>
2025-11-08 14:56:16 +0530

    Simplify echo commands in bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,3 @@
-echo '"clear"'
-echo "Welcome to thunderbash" >> ~/.bashrc
+echo clear >> ~/.bashrc
 echo "viu logo_thundershell.png" >> ~/.bashrc
 echo 'echo -e "\e[32m$(figlet ThunderBash)\e[0m"' >> ~/.bashrc
