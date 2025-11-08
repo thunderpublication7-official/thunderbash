1f9f341630d3ef990b6e05ee66484577fa616c4c
thunderpublication7-official <thunderpublication218@gmail.com>
2025-11-08 14:50:26 +0530

    Update script.sh to correct bash.sh execution
    Fix script to ensure bash.sh is executable before running.

--- script.sh
+++ script.sh
@@ -4,10 +4,10 @@ apt upgrade -y
 pkg install figlet
 pkg install viu
 wget https://raw.githubusercontent.com/thunderpublication7-official/thunderbash/refs/heads/main/bash.sh
-chmod +x bash.sh
-./bash.sh
 wget https://raw.githubusercontent.com/thunderpublication7-official/thunder-bash-site/main/logo_thundershell.png
+chmod +x bash.sh
 viu logo_thundershell.png
+./bash.sh
 echo -e "\033[0;32m$(figlet 'thunder Bash')\033[0m"
 pkg install proot-distro -y
 pd install debian
