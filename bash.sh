7912edad4a4e7d5ee8b2c21082cea9bf182b1fa2
thunderpublication7-official <thunderpublication218@gmail.com>
2025-11-08 14:46:35 +0530

    Append messages to .bashrc instead of overwriting

--- bash.sh
+++ bash.sh
@@ -1,3 +1,3 @@
-echo "Welcome to thunderbash" > ~/.bashrc
-echo "viu logo_thundershell.png" > ~/.bashrc
-echo 'echo -e "\e[32m$(figlet ThunderBash)\e[0m"' > ~/.bashrc
+echo "Welcome to thunderbash" >> ~/.bashrc
+echo "viu logo_thundershell.png" >> ~/.bashrc
+echo 'echo -e "\e[32m$(figlet ThunderBash)\e[0m"' >> ~/.bashrc
