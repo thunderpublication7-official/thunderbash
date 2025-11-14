38faf25afa5f4b9eff2247e2c8b7591227f517bd
thunderpublication7-official <thunderpublication218@gmail.com>
2025-11-14 17:48:19 +0530

    Refactor bash.sh to simplify .bashrc updates
    Removed redundant echo commands and streamlined the addition of clear and logo display commands to .bashrc.

--- bash.sh
+++ bash.sh
@@ -1,6 +1,4 @@
-echo "./bash.sh" >> ~/.bashrc
-echo "clear" >> ~/.bashrc
-echo ";" >> ~/.bashrc
-echo "viu logo_thundershell.png" >> ~/.bashrc
-echo ";" >> ~/.bashrc
+
+echo "clear;" >> ~/.bashrc
+echo "viu logo_thundershell.png;" >> ~/.bashrc
 echo 'echo -e "\e[32m$(figlet ThunderBash)\e[0m"' >> ~/.bashrc
